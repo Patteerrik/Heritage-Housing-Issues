c80927c29c5263f8be68c6ce31407c30b46d5acb
Patteerrik . <patrik.akerbergz@gmail.com>
2024-12-02 10:09:39 +0000

    Add Streamlit pages and navigation setup

---
+++ app_pages/multipage.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+class MultiPage:
+    def __init__(self, app_name):
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="📊"  # You can change the icon to personalize your app
+        )
+
+    def add_page(self, title, func):
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Select Page', self.pages, format_func=lambda page: page['title'])
+        page['function']()
